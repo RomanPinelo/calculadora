8d357f8a1243902e9471b75025bd51a305e15a09
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-07-19 10:44:59 -0500

    Elementos del DOM guardados en variables

---
+++ calculadora.js
@@ -0,0 +1,67 @@
+// Elementos del DOM
+var display = document.getElementById("display");
+
+var btn_cero = document.getElementById("cero");
+var btn_uno = document.getElementById("uno");
+var btn_dos = document.getElementById("dos");
+var btn_tres = document.getElementById("tres");
+var btn_cuatro = document.getElementById("cuatro");
+var btn_cinco = document.getElementById("cinco");
+var btn_seis = document.getElementById("seis");
+var btn_siete = document.getElementById("siete");
+var btn_ocho = document.getElementById("ocho");
+var btn_nueve = document.getElementById("nueve");
+var btn_suma = document.getElementById("suma");
+var btn_resta = document.getElementById("resta");
+var btn_multi = document.getElementById("multi");
+var btn_division = document.getElementById("division");
+var btn_potencia = document.getElementById("potencia");
+var btn_raiz = document.getElementById("raiz");
+var btn_punto = document.getElementById("punto");
+var btn_porciento = document.getElementById("porciento");
+var btn_igual = document.getElementById("igual");
+
+// eventos de los botones
+btn_cero.addEventListener('click', function(){
+  
+});
+
+btn_uno.addEventListener('click', function(){
+  
+});
+
+btn_dos.addEventListener('click', function(){
+  
+});
+
+btn_tres.addEventListener('click', function(){
+  
+});
+
+btn_cuatro.addEventListener('click', function(){
+  
+});
+
+btn_cinco.addEventListener('click', function(){
+  
+});
+
+btn_seis.addEventListener('click', function(){
+  
+});
+
+btn_siete.addEventListener('click', function(){
+  
+});
+
+btn_ocho.addEventListener('click', function(){
+  
+});
+
+btn_nueve.addEventListener('click', function(){
+  
+});
+
+// .addEventListener('click', function(){
+  
+// });
